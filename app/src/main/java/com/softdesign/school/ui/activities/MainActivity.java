b18af2c8008b87ed616dd9a5d201ffb3aa111891
badver <badver@mail.ru>
2016-01-26 21:57:57 +0400

    add onClick and isntanceState

--- app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/school/ui/activities/MainActivity.java
@@ -1,56 +1,84 @@
 package com.softdesign.school.ui.activities;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.softdesign.school.R;
 import com.softdesign.school.utils.Lg;
 
-public class MainActivity extends AppCompatActivity {
-    CheckBox mCheckBox;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private String TAG;
+    private CheckBox mCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Lg.i(this.getClass().getSimpleName(), "onCreate");
+        TAG = this.getClass().getSimpleName();
+        Lg.i(TAG, "onCreate");
         setContentView(R.layout.activity_main);
         mCheckBox = (CheckBox) findViewById(R.id.checkBox);
+        mCheckBox.setOnClickListener(this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Lg.i(this.getClass().getSimpleName(), "onStart");
+        Lg.i(TAG, "onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Lg.i(this.getClass().getSimpleName(), "onResume");
+        Lg.i(TAG, "onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Lg.i(this.getClass().getSimpleName(), "onPause");
+        Lg.i(TAG, "onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Lg.i(this.getClass().getSimpleName(), "onStop");
+        Lg.i(TAG, "onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Lg.i(this.getClass().getSimpleName(), "onDestroy");
+        Lg.i(TAG, "onDestroy");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Lg.i(this.getClass().getSimpleName(), "onRestart");
+        Lg.i(TAG, "onRestart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        switch (id) {
+            case R.id.checkBox:
+                Toast.makeText(this, "Click!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Lg.i(TAG, "onSaveInstanceState");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Lg.i(TAG, "onRestoreInstanceState");
     }
 }
